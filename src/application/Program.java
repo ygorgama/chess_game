3e71b1aeec1e76ea809169b64fde047429e7f452
Ygor Pinto Gama <ygor.bio.2002@gmail.com>
2023-07-06 11:09:40 -0300

    Class position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import bordergame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Position pos = new Position(1, 3);
+		
+		System.out.println(pos.toString());
+	}
+
+}
